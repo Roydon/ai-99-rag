4c32dc65e2193c2ee41518da437de8dd1125140f
Roydon <roydon.pereira@rablighting.com>
2025-01-29 09:12:41 -0500

    adding langchain-community package

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 import os
 import re
 
